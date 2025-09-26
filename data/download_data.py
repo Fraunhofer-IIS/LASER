4dbcdb34b2ee84c85571c0da16877adc9da8eaff
lweber <lucas.weber@iis.fraunhofer.de>
2025-09-26 16:02:33 +0200

    Add data download script

--- data/download_data.py
+++ data/download_data.py
@@ -3,3 +3,150 @@
 # 2. save the preprocessed datasets in a folder called "data"
 # 3. link this data folder in the scripts
 
+# Find info:
+# agent instruct sampling with SEED?
+
+import json
+import requests
+from datasets import load_dataset
+
+
+datapoint_id_pattern = "{dataset_id}_v{version}_{language}_{datapoint_id}"
+language = "en"
+version = "1"
+
+DATASETS = {
+            "alpaca_gpt4":                      {"hf_name": "vicgalle/alpaca-gpt4",
+                                                 "input_key": "input"}, 
+            
+            "open_math_instruct_2":             {"hf_name": "nvidia/OpenMathInstruct-2",
+                                                 "hf_kwargs": {"split": "train_2M",
+                                                             "streaming": True},
+                                                 "preprocessing": lambda x: x.filter(lambda example: "augmented" not in example["problem_source"]),
+                                                 "instruction_key": "problem",
+                                                 "output_key": "generated_solution"}, 
+            
+            "flan_v2_90k":                      {"hf_name": "ai2-adapt-dev/flan_v2_converted"},
+            
+            "sharegpt_en":                      {"hf_name": "theblackcat102/sharegpt-english",
+                                                 "messages_key": "conversations",
+                                                 "role_key": "user",
+                                                 "content_key": "text"}, 
+            
+            "wizardlm_evol_instruct":           {"hf_name": "WizardLMTeam/WizardLM_evol_instruct_V2_196k",
+                                                 "messages_key": "conversations",
+                                                 "role_key": "from",
+                                                 "content_key": "value"},
+            
+            "emnlp25_200k_agentinst_random":    {"hf_name": "microsoft/orca-agentinstruct-1M-v1",
+                                                 "preprocessing": lambda x: x.select(range(200000))}, 
+            
+            "ifeval_like_5k":                   {"hf_name": "HuggingFaceH4/ifeval-like-data"},
+
+            
+            "ultrainteract_coding":             {"hf_name": "openbmb/UltraInteract_sft",
+                                                 "output_key": "response",
+                                                 "preprocessing": lambda x: x.filter(lambda example: example["task"] == "Coding")},
+            
+            "tulu_3_sft_mixture_0225":          {"hf_name": "allenai/tulu-3-sft-mixture-0225"},
+
+            "daring_anteater":                  {"hf_name": "nvidia/Daring-Anteater",
+                                                 "messages_key": "conversations",
+                                                 "role_key": "from",
+                                                 "content_key": "value"}, 
+            
+            "conifer_v1":                       {"hf_name": "ConiferLM/Conifer",
+                                                 "hf_kwargs": {"split": "train_sft"}}, 
+            
+            "numina_math_cot_v1":               {"hf_name": "AI-MO/NuminaMath-CoT",
+                                                 "preprocessing": lambda x: x.select(range(250000))}, 
+            
+            "longform":                         {"hf_name":"akoksal/LongForm",
+                                                 "instruction_key": "input",}, 
+            
+            "dolly_15k":                        {"hf_name": "databricks/databricks-dolly-15k",
+                                                 "input_key": "context",
+                                                 "output_key": "response"},
+            
+            "alpaca":                           {"url": "https://raw.githubusercontent.com/tatsu-lab/stanford_alpaca/main/alpaca_data.json",
+                                                 "input_key": "input"}, 
+}
+
+def convert_to_chat_format(data):
+        converted = []
+        for sample in data:
+            if "messages" in sample:
+                sample.pop("messages")
+            instruction = sample.pop("instruction")
+            response = sample.pop("output")
+            converted.append({"messages":
+                                [{
+                                    "content": instruction,
+                                    "role": "user"
+                                    },
+                                    {"content": response,
+                                    "role": "assistant"
+                                    }],
+                            **sample
+                            })
+        return converted
+
+for dataset_name, dataset_info in DATASETS.items():
+    if "emnlp25_200k_agentinst_random" in dataset_name:
+        continue
+    print(f"Processing dataset: {dataset_name}...")
+    
+    if "hf_name"  in dataset_info:
+        hf_kwargs = dataset_info.get("hf_kwargs", {"split": "train"})
+        
+        # Load dataset
+        data = load_dataset(dataset_info["hf_name"], **hf_kwargs)
+        
+        # Uniformize column names
+        if "instruction_key" in dataset_info:
+            data = data.rename_column(dataset_info["instruction_key"], "instruction")
+        if "output_key" in dataset_info:
+            data = data.rename_column(dataset_info["output_key"], "output")
+        if "input_key" in dataset_info:
+            data = data.map(lambda x: {**x,"instruction": (x["instruction"] + "\n" + x[dataset_info["input_key"]]).strip(),})
+        if "messages_key" in dataset_info:
+            data = data.rename_column(dataset_info["messages_key"], "messages")
+            for sample in data:
+                for i, message in enumerate(sample["messages"]):
+                    if dataset_info["role_key"]:
+                        message["role"] = ["user", "assistant"][i % 2]
+                    if dataset_info["content_key"]:
+                        message["content"] = message.pop(dataset_info["content_key"])
+                        
+        # Preprocess
+        if "preprocessing" in dataset_info:
+            data = dataset_info["preprocessing"](data)
+        
+        # Convert to list of dicts
+        data = [x for x in data]
+    
+    elif "url" in dataset_info:
+        response = requests.get(dataset_info["url"])
+    
+        if response.status_code == 200:
+            data = response.json()
+            
+        if "input_key" in dataset_info:
+            data = [{**sample,"instruction": (sample["instruction"] + "\n" + sample[dataset_info["input_key"]]).strip()} for sample in data]
+       
+    # Add unique data IDs
+    data = [{**sample, "data_id": datapoint_id_pattern.format(dataset_id=dataset_name.replace("_", "-"), version=version, language=language, datapoint_id=i)} for i, sample in enumerate(data)]
+    
+    # Convert to chat format
+    if "messages" not in data[0].keys():
+        data = convert_to_chat_format(data)
+        
+    # Keep only relevant columns
+    data = [{"messages": sample["messages"], "data_id": sample["data_id"]} for sample in data]
+        
+    
+    output_path = f"{dataset_name}.jsonl"
+    with open(output_path, 'w') as file:
+        for line in data:
+            _ = file.write(json.dumps(line) + '\n')
+    
--- scripts/analyse_data.sh
+++ scripts/analyse_data.sh
@@ -2,6 +2,7 @@
 
 cd ../analysis
 
+# source dataset main experiments
 DATASETS="alpaca_gpt4,open_math_instruct_2,flan_v2_90k,sharegpt_en,wizardlm_evol_instruct,colm25_200k_agentinst_random,ifeval_like_5k,ultrainteract_coding"
 
 python run_analysis --dataset ${DATASETS} --analysis tokens --request_batch_size 8
@@ -11,3 +12,22 @@ python run_analysis --dataset ${DATASETS} --analysis quality --request_batch_siz
 python run_analysis --dataset ${DATASETS} --analysis if_quality --request_batch_size 8
 python run_analysis --dataset ${DATASETS} --analysis code_quality --request_batch_size 8
 
+# source dataset (strong)
+DATASETS="tulu_3_sft_mixture_0225"
+
+python run_analysis --dataset ${DATASETS} --analysis tokens --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis categories_v2 --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis difficulty_v2 --request_batch_size 8 
+python run_analysis --dataset ${DATASETS} --analysis quality --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis if_quality --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis code_quality --request_batch_size 8
+
+# source dataset (weak)
+DATASETS="alpaca_gpt4,flan_v2_90k,alpaca,daring_anteater,conifer_v1,numina_math_cot_v1,longform,dolly_15k"
+
+python run_analysis --dataset ${DATASETS} --analysis tokens --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis categories_v2 --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis difficulty_v2 --request_batch_size 8 
+python run_analysis --dataset ${DATASETS} --analysis quality --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis if_quality --request_batch_size 8
+python run_analysis --dataset ${DATASETS} --analysis code_quality --request_batch_size 8
